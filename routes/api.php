e478f8d7a0e792001cc21ba83d981dc066533e88
aungchannmyae <myae0517@gmail.com>
2026-02-04 23:11:59 +0630

    feat/customer created

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -17,4 +18,5 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::patch('/change-password', 'changePassword');
         Route::patch('/change-name', 'changeName');
     });
+    Route::apiResource('customers', CustomerController::class);
 });
